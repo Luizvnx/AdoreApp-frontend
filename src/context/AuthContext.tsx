3a91fffc8009000e4a4c19cd53f228a00825e811
Luiz <luizvnx@gmail.com>
2026-08-19 01:06:27 -0300

    Fix UserRole

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { api } from '../services/api';
-import type { User } from '../types';
+import type { User, UserRole } from '../types';
 
 interface AuthContextData {
   user: User | null;
